5589a888af2e0400f00716729da5369e2ee00078
Maryluz García <ruthmaryroot@gmail.com>
2023-09-23 13:14:07 -0600

    Clase orderHasProduct

---
+++ backendProject/src/main/java/com/bubblebear/backendProject/entity/OrdersHasProduct.java
@@ -0,0 +1,20 @@
+package com.bubblebear.backendProject.entity;
+
+import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+import lombok.Getter;
+//import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+@ToString
+@Table(name = "orders_has_products")
+
+public class OrdersHasProduct {
+
+}
