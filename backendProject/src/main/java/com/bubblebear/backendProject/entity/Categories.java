2a4e7ca234b85f905b41a2ade2cdc61ba156bd35
luis muciño <nievesmucino@gmail.com>
2023-09-22 19:00:02 -0600

    iniciando entidades

---
+++ backendProject/src/main/java/com/bubblebear/backendProject/entity/Categories.java
@@ -0,0 +1,29 @@
+package com.bubblebear.backendProject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="Categories")
+
+public class Categories {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "categories_id")
+	private long id;
+}
---
+++ backendProject/src/main/java/com/bubblebear/backendProject/entity/User.java
@@ -0,0 +1,17 @@
+package com.bubblebear.backendProject.entity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="users")
+public class User {
+
+
+
+}
