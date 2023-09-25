c5126b2c514d8977a48bf71c565f7390d6b416e3
luis muciño <nievesmucino@gmail.com>
2023-09-25 16:40:43 -0600

    ...

--- backendProject/src/main/java/com/bubblebear/backendProject/controller/CategoriesController.java
+++ backendProject/src/main/java/com/bubblebear/backendProject/controller/CategoriesController.java
@@ -5,6 +5,8 @@ package com.bubblebear.backendProject.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,8 +43,9 @@ public class CategoriesController {
 	
 	
 	@PostMapping()
-	public Categories createCategory( @RequestBody Categories createdCategory) {
-		return createdCategory;
+	public ResponseEntity<Categories> createCategory( @RequestBody Categories category) {
+		Categories savedCategory = categoriesRepository.save(category);
+		return new ResponseEntity<>(savedCategory, HttpStatus.CREATED) ;
 	}
 	
 	
