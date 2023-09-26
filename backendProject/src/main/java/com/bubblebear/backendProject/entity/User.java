fe692ab3e25b93dd58cf15774417b5060e8f5a88
luis muciño <nievesmucino@gmail.com>
2023-09-25 18:01:06 -0600

    users merge

--- backendProject/src/main/java/com/bubblebear/backendProject/entity/User.java
+++ backendProject/src/main/java/com/bubblebear/backendProject/entity/User.java
@@ -1,15 +1,10 @@
 package com.bubblebear.backendProject.entity;
-<<<<<<< HEAD
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-=======
 
 import java.util.Date;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
->>>>>>> 89c095fbc5cd92b3619526fec35f5aefd29a9726
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -18,18 +13,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-
-//@AllArgsConstructor
-//@NoArgsConstructor
 @Entity
-<<<<<<< HEAD
-@Data
-//@Table(name="users")
-public class User {
-	@Id
-	@Column(name="user_id")
-	private long id;
-=======
 @Getter
 @Setter
 @AllArgsConstructor
@@ -61,7 +45,6 @@ public class User {
 	
 	@Column(name = "role")
 	private Boolean role;
->>>>>>> 89c095fbc5cd92b3619526fec35f5aefd29a9726
 
 	
 	//@OneToMany(mapperdBy = "users")  @JsonIgnoreProperties("users");
